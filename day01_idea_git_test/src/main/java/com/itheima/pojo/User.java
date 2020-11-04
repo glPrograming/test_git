d55eacaa08baa4ccf8be9c9e126667c105e38d0e
gl_study <2195879375@qq.com>
2020-11-04 11:25:48 +0800

    第一次提交, module idea-git-test

---
+++ day01_idea_git_test/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,26 @@
+package com.itheima.pojo;
+
+/**
+ * @Author: gl
+ * @Date: 2020/11/4 10:33
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
